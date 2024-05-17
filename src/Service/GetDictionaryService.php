ed2e9342c140eda6c5086f094707ec882cf58412
aleksandr sidorkin <sanyok1234@rambler.ru>
2024-05-17 12:57:46 +0300

    commit

--- src/Service/GetDictionaryService.php
+++ src/Service/GetDictionaryService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\Model\DictionaryItem;
 use SplObjectStorage;
+use Smalot\PdfParser\Parser;
 
 class GetDictionaryService
 {
@@ -41,11 +42,35 @@ class GetDictionaryService
 
     protected function retrievePDFFile()
     {
+        $url = 'https://mspu.gov.ua/storage/app/sites/17/%D0%BE%D0%B1%D0%BE%D1%80%D0%BE%D0%BD%D0%BD%D1%96%20%D0%B7%D0%B0%D0%BA%D1%83%D0%BF%D1%96%D0%B2%D0%BB%D1%96%20%D1%84%D0%B0%D0%B9%D0%BB%D0%B8/dk021-2015.pdf';
 
+        $fileContent = file_get_contents($url);
+        if ($fileContent === FALSE) {
+            throw new \Exception("Не удалось скачать PDF файл.");
+        }
+        return $fileContent;
+    
     }
 
     protected function parsePDFFile($pdf): array
     {
-
+        $parser = new Parser();
+        $pdf = $parser->parseContent($pdf);
+        $text = $pdf->getText();
+            // echo  $text;
+        $lines = explode("\n", $text);
+        $dataArray = [];
+    
+        foreach ($lines as $line) {
+            $parts = explode(" ", $line, 2);
+            
+            if (count($parts) == 2) {
+                $code = trim($parts[0]);
+                $description = trim($parts[1]);
+                $dataArray[] = ["code" => $code, "description" => $description];
+            }
+        }
+    
+        return $dataArray;
     }
 }
